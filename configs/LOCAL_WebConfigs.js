3fe44098058974b5a333a255369db00d61e96e99
Bivas Naik <bivascareers@gmail.com>
2025-10-01 21:26:47 +0530

    Made Configuration more modularized

---
+++ configs/LOCAL_WebConfigs.js
@@ -0,0 +1,96 @@
+// @ts-check
+import { devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export const Browsers_n_Device_Setup = {
+  testDir: './tests/BnTests/',
+  
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+
+  //reporter
+  reporter: 'html',
+
+  //Timeout
+  timeout: 30 * 1000,               // 30s - overall test timeout
+  expect: {
+    timeout: 5 * 1000,              // 5s - expect() assertion timeout
+  },
+
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  use: {
+    //On Navigation or Action timeout
+    navigationTimeout: 15 * 1000,   // 15s - navigation timeout (e.g., page.goto)
+    actionTimeout: 10 * 1000,       // 10s - per action timeout (e.g., click, fill)
+
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Headless mode */
+    headless: false,
+    // launchOptions: {
+    //   headless: false,   // ✅ Correct place for headless
+    // },
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    screenshot: 'only-on-failure',
+    video: 'on',
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+};
+
---
+++ configs/index.js
@@ -0,0 +1,4 @@
+import * as LocalWebConfig from './LOCAL_WebConfigs.js';
+import * as CIWebConfig from './CI_WebConfigs.js';
+
+export { LocalWebConfig, CIWebConfig };
